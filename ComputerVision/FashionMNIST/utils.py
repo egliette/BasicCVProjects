6f4a5ebf1bb2ef4f90db514e88ddd5b5e7ea37d0
Tu Le Minh <leminhtu74@gmail.com>
2022-09-03 12:09:20 +0700

    add tensorboard

--- ComputerVision/FashionMNIST/utils.py
+++ ComputerVision/FashionMNIST/utils.py
@@ -1,7 +1,9 @@
 import os
 import shutil
+import matplotlib.pyplot as plt
 
 import torch
+import torch.nn.functional as F
 import torchvision
 import pandas as pd
 
@@ -39,3 +41,31 @@ def download_fmnist_img_data(is_training=True, save_dir="data/"):
 def one_hot_transform(label, num_classes):
     label_tensor = torch.Tensor([label]).long()
     return torch.nn.functional.one_hot(label_tensor, num_classes=num_classes)
+
+def plot_predictions_vs_labels(images, labels, outputs, predictions, labels_map):
+    """"
+    Return figure which shows 5 images with corresponding labels and predictions
+    @param images (Tensor): batch of input images
+    @param labels (Tensor): batch of labels
+    @param outputs (Tensor): outputs of model(images)
+    @param predictions (Tensor): argmax of outputs
+    @return fig (figure): figure which shows 5 images with corresponding labels and predictions
+    """
+    probs = F.softmax(outputs)
+    fig = plt.figure(figsize=(10, 10))
+    for i in range(5):
+        ith_label = labels_map[labels[i].cpu().item()]
+        ith_pred = labels_map[predictions[i].item()]
+        ith_prob = probs[i][predictions[i]].item()
+        ax = fig.add_subplot(1, 5, i+1, xticks=[], yticks=[])
+        plt.tight_layout()
+        plt.imshow(images[i].cpu().squeeze(), cmap="gray")
+        ax.set_title(
+            "Label {0}\nPred {1}: {2:.1f}".format(
+                ith_label,
+                ith_pred,
+                ith_prob
+            ),
+            color="green" if predictions[i]==labels[i] else "red"
+        )
+    return fig
